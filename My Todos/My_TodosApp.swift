28a9e5107357bcba9e7fa8deac11b7f8902d650b
aninhabort <carol2015bortolini@gmail.com>
2024-10-13 18:25:37 -0300

    Initial Commit

---
+++ My Todos/My_TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_TodosApp.swift
+//  My Todos
+//
+//  Created by Ana Carolina B. de Magalhães on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct My_TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ My TodosTests/My_TodosTests.swift
@@ -0,0 +1,17 @@
+//
+//  My_TodosTests.swift
+//  My TodosTests
+//
+//  Created by Ana Carolina B. de Magalhães on 13/10/24.
+//
+
+import Testing
+@testable import My_Todos
+
+struct My_TodosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
